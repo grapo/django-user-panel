debee62eccfcf934a1b36e4c9a8e8d948c28d882
Piotr Grabowski <grabowskiptr@gmail.com>
2018-04-25 10:12:49 +0200

    add compatibility for Django 2.0

--- user_panel/panels.py
+++ user_panel/panels.py
@@ -15,7 +15,7 @@ class UserPanel(Panel):
 
     @property
     def nav_subtitle(self):
-        if self.toolbar.request.user.is_authenticated():
+        if self.toolbar.request.user.is_authenticated:
             return self.toolbar.request.user
         else:
             return _('Not authenticated')
@@ -33,7 +33,7 @@ class UserPanel(Panel):
         User = get_user_model()
         redirect_to = request.GET.get(REDIRECT_FIELD_NAME, '')
 
-        if hasattr(request, 'user') and request.user.is_authenticated():
+        if hasattr(request, 'user') and request.user.is_authenticated:
             for field in User._meta.fields:
                 current.append(
                     (field.attname, getattr(request.user, field.attname)))
